fb0abf2c0815fa0ceff03da9293653197cb978e0
Jamie Land <18668469@students.lincoln.ac.uk>
2018-10-06 22:12:55 +0100

    Made a start on top 15
    Moved the lists to become static for the program

--- Weekend Box Office/Weekend Box Office/Program.cs
+++ Weekend Box Office/Weekend Box Office/Program.cs
@@ -9,25 +9,50 @@ namespace Weekend_Box_Office
 {
     class Program
     {
-        static void top_15()
+        static List<string> l_rank = new List<string>(); // The films ranking in the list.
+        static List<string> l_film = new List<string>(); // The name of the film.
+        static List<string> l_origin = new List<string>(); // The country of origin for the film.
+        static List<string> l_weekend_gross = new List<string>(); // The weekend gross of the film.
+        static List<string> l_distributor = new List<string>(); // The distributor of the film.
+        static List<string> l_change = new List<string>(); // The percentage of change in the last week.
+        static List<string> l_weeks_on_release = new List<string>(); // The weeks since the release of the film.
+        static List<string> l_cinemas = new List<string>(); // The number of cinemas which showed the film.
+        static List<string> l_site_average = new List<string>(); // The average of money earned per site.
+        static List<string> l_total_gross = new List<string>(); // The total gross to date.
+
+        // Calculate the average of "weekend gross" for all top 15 films.
+        static void Top_15(List<string> rank, List<string> gross)
         {
-            // Calculate the average of "weekend gross" for all top 15 films.
-            Console.WriteLine();
+            double total = 0;
+            
+            // Going through every line of the loaded csv.
+            for (int i = 0; i <= rank.Count(); i++)
+            {
+                // If the rank is 1 through 15...
+                if (Int32.Parse(rank[i]) < 16)
+                {
+                    // Add it to the total
+                    total += Double.Parse(gross[i]); 
+                }
+            }
+
+            // All them added up divided by the amount of films.
+            Console.WriteLine("The weekend gross of the top 15 films is: £{0}", total / 15);
         }
 
-        static void all_UK_USA()
+        static void All_UK_USA()
         {
             // Calculates the average ‘weekend gross’ of all UK/USA origin films
             Console.WriteLine();
         }
 
-        static void how_many_viewings()
+        static void How_many_viewings()
         {
             // Given an average ticket price of £8.00, how many ‘viewings’ of ‘Disney’s Christopher Robin’ were made in this week? 
             Console.WriteLine();
         }
 
-        static void previous_week()
+        static void Previous_week()
         {
             //  The ‘weekend gross’ figures for ‘Mamma Mia: Here We Go Again’ and ‘Disney’s Christopher Robin’ for the previous week. 
             Console.WriteLine();
@@ -38,18 +63,7 @@ namespace Weekend_Box_Office
             // Reading the XLS file which has been converted into a .csv file
             // I have also removed the headers and extra data we don't need from the given file
             using (var reader = new StreamReader(@"Weekend Box Office.csv"))
-            {
-                List<string> l_rank = new List<string>(); // The films ranking in the list.
-                List<string> l_film = new List<string>(); // The name of the film.
-                List<string> l_origin = new List<string>(); // The country of origin for the film.
-                List<string> l_weekend_gross = new List<string>(); // The weekend gross of the film.
-                List<string> l_distributor = new List<string>(); // The distributor of the film.
-                List<string> l_change = new List<string>(); // The percentage of change in the last week.
-                List<string> l_weeks_on_release = new List<string>(); // The weeks since the release of the film.
-                List<string> l_cinemas = new List<string>(); // The number of cinemas which showed the film.
-                List<string> l_site_average = new List<string>(); // The average of money earned per site.
-                List<string> l_total_gross = new List<string>(); // The total gross to date.
-
+            { 
                 // Keeps going until there is no more left to read
                 while (!reader.EndOfStream)
                 {
@@ -72,6 +86,8 @@ namespace Weekend_Box_Office
                 }
                 
             }
+            
+            Top_15(l_rank, l_weekend_gross);
         }
     }
 }
